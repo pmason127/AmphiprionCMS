347f755bea185e28df3cbdad7dc9eaab1ab041fd
pmason127 <pmason127@users.noreply.github.com>
2014-05-20 16:23:53 -0500

    Test

--- AmphiprionCMS/Code/DateTimeModelBinder.cs
+++ AmphiprionCMS/Code/DateTimeModelBinder.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-//DateTime?
+
 namespace AmphiprionCMS.Code
 {
     public class DateTimeModelBinder : IModelBinder
